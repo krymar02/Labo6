66ccbc1414f5553820f3c892e8957c2d354e1210
Kryssia Martinez <130623878+krymar02@users.noreply.github.com>
2023-12-02 21:13:30 -0600

    Add files via upload
    Se completo el archivo brindado con las operacioones y agregue la función def ejecutar_operacion

---
+++ calculadora.py
@@ -0,0 +1,43 @@
+operations = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x / y,
+}
+def get_user_input():
+    try:
+        num1 = float(input("Ingrese un numero: "))
+        num2 = float(input("Ingrese otro numero: "))
+        operation = input("Elija una operacion (+, -, *, /) o escriba 'exit' para salir: ")
+        return num1, num2, operation
+    except ValueError:
+        print("Input invalido. Por favor ingrese numeros.")
+        return get_user_input()
+
+def ejecutar_operacion(user_input, callback):
+    num1, num2, operation = user_input
+    if operation in operations:
+        result = operations[operation](num1, num2)
+    else:
+        result = "Operación invalida"
+
+    print("Resultado:", result)
+
+def main():
+    while True:
+        user_input = get_user_input()
+
+        if user_input[2].lower() == 'exit':
+            print("Salir.")
+            break
+
+        print("\nCalculando...")
+
+        if user_input[2] in operations:
+            ejecutar_operacion(user_input, operations[user_input[2]])
+        else:
+            print("Operacion invalida. Seleccione (+, -, *, /) o  escriba 'exit' para salir.")
+
+if __name__ == "__main__":
+    main()
+
